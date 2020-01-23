2c0b85364de2b22df12fadd46e3a587d38d1d157
joannaotmianowska <otmianowska.joanna@gmail.com>
2020-01-23 15:59:15 +0100

    candidate intro shown on the list &  UTs for data display

--- src/components/CandidateIntro/CandidateIntro.test.js
+++ src/components/CandidateIntro/CandidateIntro.test.js
@@ -3,8 +3,27 @@ import { render } from '@testing-library/react';
 import CandidateIntro from './CandidateIntro';
 
 describe('CandidateIntro component', () => {
-    test('renders candidate details', () => {
-        // TO DO - check data display
+    const mockIntro = {
+        name: {
+            first: "abc",
+            last: "cde"
+        },
+        location: {
+            country: "efg",
+            city: "ijk"
+        },
+        picture: {
+            thumbnail: "https://examplarypic.co"
+        }
+    }
 
+    test('renders candidate details', () => {
+        const { getByText, getByAltText } = render(<CandidateIntro details={mockIntro}/>);
+
+        expect(getByText(mockIntro.name.first)).toBeInTheDocument();
+        expect(getByText(mockIntro.name.last)).toBeInTheDocument();
+        expect(getByText(mockIntro.location.country)).toBeInTheDocument();
+        expect(getByText(mockIntro.location.city)).toBeInTheDocument();
+        expect(getByAltText(mockIntro.name.first)).toBeInTheDocument();
     });
 });
